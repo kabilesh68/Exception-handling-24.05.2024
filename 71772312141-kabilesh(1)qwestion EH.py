051055caefde8e89b5f83d1ec87aad1716164dc4
kabilesh68 <168735849+kabilesh68@users.noreply.github.com>
2024-05-24 10:00:04 +0530

    Update 71772312141-kabilesh(1)qwestion EH.py

--- 71772312141-kabilesh(1)qwestion EH.py
+++ 71772312141-kabilesh(1)qwestion EH.py
@@ -7,3 +7,12 @@ try:
              raise ValueError('Negative number')
 except ValueError as ve :
    print(ve.args)
+output:
+Enter a positive number: 88
+7744
+Enter a positive number: 6
+36
+Enter a positive number: 78
+6084
+Enter a positive number: -8
+('Negative number',)
